f540921b6af6e9d525f801704a41e8cd14459931
meocutcut <thangngoc_041291@yahoo.com.vn>
2013-09-19 16:46:43 +0700

    Edit web.js and add index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fss = require('graceful-fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    
+  response.send(fss.readFileSync("index.html").toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
