8ba0adb621ef420a247824b990a0607e549b3cba
meocutcut <thangngoc_041291@yahoo.com.vn>
2013-09-20 15:23:24 +0700

    Edited index2.html from develop branch

--- grader.js
+++ grader.js
@@ -55,6 +55,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtml = function(html, checksfile){
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +76,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>','Url to use')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+	var rest = require('restler');
+
+	rest.get(program.url).on('complete', function(result) {
+	        var checkJson = checkHtml(result, program.checks);
+	        var outJson = JSON.stringify(checkJson, null, 4);
+	        console.log(outJson);
+	        });
+	}
+    else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var fss = require('graceful-fs');
+
+
 var app = express();
 app.use(express.logger());
 
@@ -12,3 +14,18 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/*
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response){
+var html = fs.readFileSync(htmlfile).toString();
+response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+console.log("Listening on" + port);
+});
+*/
